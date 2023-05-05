4350ba27f09a3aaa0708b8bf07d8c8b9e289216b
logan <lkostick1@gmail.com>
2023-05-05 16:08:05 -0400

    Add padding parameter as 4th arg

--- rand_mac_ping.py
+++ rand_mac_ping.py
@@ -4,7 +4,8 @@ import sys
 dst_ip    = sys.argv[1]
 count     = int(sys.argv[2])
 data_rate = int(sys.argv[3])
+padding      = int(sys.argv[4])
 
 src_mac = RandMAC()
 ip_src = RandIP()
-sendp(Ether(src=src_mac)/IP(src=ip_src,dst=dst_ip)/ICMP(), count=count, inter=(data_rate * (10**-6)))
+sendp(Ether(src=src_mac)/IP(src=ip_src,dst=dst_ip)/ICMP()/Raw(RandString(size=padding)), count=count, inter=(data_rate * (10**-6)))
